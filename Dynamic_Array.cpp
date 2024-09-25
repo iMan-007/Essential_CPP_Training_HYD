786a49a8567a68e48eee6d4107a6c31f3d0fad9e
iMan-007 <manikandanest@gmail.com>
2024-09-25 09:47:47 +0530

    Create Dynamic_Array.cpp

---
+++ Dynamic_Array.cpp
@@ -0,0 +1,54 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class DynamicArray{
+    private:
+    int index, count;
+    int* array = new int[0];
+    
+    public:
+    
+    void add(int value){
+        count++;
+        if(index<=count)
+        {
+           int* array = new int[index]; 
+        }
+        this->array[index] = value;
+    }
+    
+    DynamicArray(int indexArg)
+    {
+       this->index= indexArg;
+       int* array = new int[indexArg];
+       
+    }
+    
+     
+friend std::ostream& operator<<(std::ostream& os, const DynamicArray& numbersArg);
+    
+};
+
+std::ostream& operator<<(std::ostream& os, const DynamicArray& numbersArg){
+            for (int i = 0; i<numbersArg.index; i++)
+            {
+            os<<numbersArg.array[numbersArg.index]<<", ";
+            }
+    return os;
+}
+
+
+int main(){
+    DynamicArray numbers(2);
+    numbers.add(10);
+    numbers.add(20);
+    numbers.add(30);
+    cout<<numbers<<endl; // 10,20,30
+    //int value=numbers[1];
+    //cout<<value<<endl; //20
+   return 0;
+  
+};
